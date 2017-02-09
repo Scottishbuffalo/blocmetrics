70222595186280abe60f38694ed13f09e159839b
Mike MacAdam <mikemacadam87@gmail.com>
2017-02-09 12:02:12 -0800

    checkpoint-3

--- Blocmetrics/db/schema.rb
+++ Blocmetrics/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209193405) do
+ActiveRecord::Schema.define(version: 20170209195528) do
+
+  create_table "applications", force: :cascade do |t|
+    t.string   "title",      default: "", null: false
+    t.string   "url",        default: "", null: false
+    t.integer  "user_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.index ["url"], name: "index_applications_on_url", unique: true
+    t.index ["user_id"], name: "index_applications_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
